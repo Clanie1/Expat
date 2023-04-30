f9fb39d034323ecd5e5d33abcab204ddfc8622d8
Calebpyn <87716503+Calebpyn@users.noreply.github.com>
2023-04-29 19:56:31 -0700

    hero

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,64 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const burgerMenu = document.querySelector('#burger-menu')!;
+const navBar = document.querySelector<HTMLDivElement>('#burger-navbar')!;
+
+const op1 = document.querySelector('#option-1') as HTMLButtonElement;
+const op2 = document.querySelector('#option-2')!;
+const op3 = document.querySelector('#option-3')!;
+const op4 = document.querySelector('#option-4')!;
+
+var state = 1;
+
+burgerMenu.addEventListener('click', () => {
+
+  navBar.classList.toggle('translate-x-[35rem]')
+
+  if(state == 1){
+    state=2;
+    op1.classList.add('text-white')
+    op1.classList.remove('text-[#FFFFFF00]')
+
+    op2.classList.add('text-white')
+    op2.classList.remove('text-[#FFFFFF00]')
+
+    op3.classList.add('text-white')
+    op3.classList.remove('text-[#FFFFFF00]')
+
+    op4.classList.add('text-white')
+    op4.classList.remove('text-[#FFFFFF00]')
+
+  }else{
+    state=1;
+    op1.classList.remove('text-white')
+    op1.classList.add('text-[#FFFFFF00]')
+    
+    op2.classList.add('text-white')
+    op2.classList.remove('text-[#FFFFFF00]')
+    
+    op3.classList.add('text-white')
+    op3.classList.remove('text-[#FFFFFF00]')
+
+    op4.classList.add('text-white')
+    op4.classList.remove('text-[#FFFFFF00]')
+  }
+
+  // if(state==1){
+  //   state=2;
+  //   console.log('ease-in')
+  //   op1.classList.replace('duration-600','duration-100')
+  //   op2.classList.replace('duration-600','duration-100')
+  //   op3.classList.replace('duration-600','duration-100')
+  //   op4.classList.replace('duration-600','duration-100')
+  // }else{
+  //   state=1;
+  //   console.log('ease-out')
+  //   op1.classList.replace('duration-100','duration-600')
+  //   op2.classList.replace('duration-100','duration-600')
+  //   op3.classList.replace('duration-100','duration-600')
+  //   op4.classList.replace('duration-100','duration-600')
+  // }
+  
+
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    backgroundSize: {
+      'cover-pic':'135%'
+    },
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif']
+      },
+      colors: {
+        'primary-blue': '#010C1EFF',
+        'sec-blue': '#06356FFF',
+        'primary-gray': '#1E1E1EFF'
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+  ],
 };
